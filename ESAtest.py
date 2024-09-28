b03049baa280f22edf121b47e5c2088e6db9842c
waltair04 <walttem@metropolia.fi>
2024-09-28 15:47:38 +0300

    Game settings

--- ESAtest.py
+++ ESAtest.py
@@ -14,3 +14,52 @@ conn = mysql.connector.connect(
     collation='utf8mb4_general_ci'
 )
 
+
+def get_airports():
+    sql = ("SELECT name, ident, type, iso_country, longitude_deg, latitude_deg "
+           "FROM airport "
+           "Where continent = 'EU' "
+           "AND type = 'large_airport'"
+           "ORDER BY RAND() LIMIT 22;")
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    return result
+
+
+def get_goals():
+    sql = ("SELECT * FROM goals;")
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    return result
+
+
+def word():
+    sql = ("select word from word_list ORDER BY RAND() LIMIT 1;")
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute(sql)
+    rows = cursor.fetchone()
+    list1 = []
+    for i in rows['word']:
+        list1.append(i)
+    return list1
+
+
+def create_game(start_money, player_points, current_airport, player_name, all_airports):
+    sql = ("INSERT INTO game (money, points, location, screen_name)"
+           "VALUES (start_money, player_points, current_airport, player_name);")
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute(sql)
+    game_id = cursor.lastrowid
+
+    goals = get_goals()
+
+
+'''name = input("Anna nimi: ")
+points = 20000
+money = 2000
+'''
+
+# print(word())
+# print(get_airports())
