954df9617624174d7438dfa40ffaa5b779f1714f
waltair04 <walttem@metropolia.fi>
2024-12-03 13:44:33 +0200

    Esa base

---
+++ esa.js
@@ -0,0 +1,6 @@
+const map = L.map('map').setView([60.23, 24.74], 13);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+
+const airportMarkers = L.featureGroup().addTo(map);
