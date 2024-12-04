feaac725f212465344256ba86a947894d5c8ce9b
waltair04 <walttem@metropolia.fi>
2024-12-04 19:28:06 +0200

    startAirport function

--- esa.js
+++ esa.js
@@ -3,36 +3,34 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
 
-const apiUrl = "http://127.0.0.1:3000/getAirports";
-// const startLocation;
-async function getStartAirport() {
-    try {
-        const response = await fetch(apiUrl);
-        if (!response.ok) {
-            throw new Error(response.status.toString());
-        }
-        const airportInfo = await response.json();
-        // const startAirport = airportInfo[0].ident;
-        console.log(airportInfo);
-    } catch (error) {
-        console.error(error.message);
-    }
-}
-
-/*
-const startAirport = await getStartAirport();
-console.log(startAirport);
-
+const apiUrl = "http://127.0.0.1:3000/";
 const airportMarkers = L.featureGroup().addTo(map);
 
 const blueIcon = L.divIcon({className: "blue-icon"});
 
-airportMarkers.clearLayers();
-const marker = L.marker([startAirport[0].latitude_deg, startAirport[0].longitude_deg]).addTo(map);
+async function getStartAirport() {
+    try {
+        const response = await fetch(apiUrl + "getAirports");
+        if (!response.ok) {
+            throw new Error(response.status.toString());
+        }
+        const airportInfo = await response.json();
+        const startAirport = airportInfo[0].ident;
+        return startAirport;
+
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+getStartAirport();
+/*
+const marker = L.marker([airportInfo[0].latitude_deg, airportInfo[0].longitude_deg]);
 airportMarkers.addLayer(marker);
 marker.setIcon(blueIcon);
-
 */
+// airportMarkers.clearLayers();
+
+/*
 (async () => {
     const startAirport = await getStartAirport();
     if (startAirport) {
@@ -42,6 +40,8 @@ marker.setIcon(blueIcon);
         marker.setIcon(blueIcon);
     }
 })();
+*/
+
 
 /*
 async function gameSetUp(){
